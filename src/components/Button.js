1e66e576a9b85b50364e656c95407dfc0012ed08
AnhQuan202 <120768861+anhquan1812@users.noreply.github.com>
2024-04-01 11:21:59 +0700

    change style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import classNames from "classnames";
-function Button({className, onClick, children}) {
-return ( <button className= {classNames('btn', className)}onClick={onClick}>
-        {children}
-    </button> );
+function Button({ className, onClick, children }) {
+  return (
+    <button className={classNames("btn", className)} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
