66606b119d716c90b227e68df718377b92b762c8
AnhQuan202 <120768861+anhquan1812@users.noreply.github.com>
2024-04-01 11:15:54 +0700

    add logout

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,12 +1,23 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 function Account() {
-  const name = sessionStorage.getItem("name");
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.removeItem('staffName');
+    alert('Bạn đã đăng xuất khỏi hệ thống')
+    navigate('/');
+  }
+  const name = sessionStorage.getItem("staffName");
   return (
-    <div className="d-flex align-items-center my-3 p-3">
+    <div className="d-flex align-items-center p-3 text-white">
       <FontAwesomeIcon icon={faUser} style={{width:'34px', height:'inherit'}}/>
       <span className="ps-3">{name}</span>
+      <div className="ms-2" onClick={handleLogout} style={{cursor:'pointer', lineHeight:3}}>
+        <FontAwesomeIcon icon={faArrowRight}/>
+        Log out
+      </div>
     </div>
   );
 }
