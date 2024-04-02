bfd422629d76f6e856736f735e519b77d459c403
AnhQuan202 <120768861+anhquan1812@users.noreply.github.com>
2024-04-02 23:19:58 +0700

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@ import Invoice from "src/pages/Invoice/Invoice";
 import CreateInvoice from "src/pages/Create/Invoice";
 import Search from "src/components/Search";
 import CreateCustomer from "src/pages/Create/Customer";
+import UpdateStatusInvoice from "src/pages/Update/Invoice";
 const publicRoutes = [
     {path: '/search', component: Search},
     {path:'/', component:Login, layout:null},
@@ -27,7 +28,8 @@ const publicRoutes = [
     {path: '/product/update/:productId', component: UpdateProduct},
     {path: '/product/updateDetail/:productId', component: UpdateProductDetail},
     {path: '/invoices', component: Invoice},
-    {path: '/invoice/create', component: CreateInvoice}
+    {path: '/invoice/create', component: CreateInvoice},
+    {path: '/invoice/updateStatus/:invoiceId', component: UpdateStatusInvoice},
 ]
 const privateRoutes = [
 
